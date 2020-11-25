af01db063d90152f63f105b32ce7d5a8486c84ca
Tom Hyde <thyde@neworbit.co.uk>
2020-11-25 18:36:30 +0000

    Initial commit

---
+++ AzureAppConfigurationExample/Controllers/WelcomeController.cs
@@ -0,0 +1,26 @@
+﻿namespace AzureAppConfigurationExample.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Configuration;
+
+    [ApiController]
+    [Route("[controller]")]
+    public class WelcomeController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+
+        public WelcomeController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            var greeting = configuration.GetValue<string>("greeting");
+            var environmentDetail = configuration.GetValue<string>("environmentDetail");
+            var key = configuration.GetValue<string>("key");
+            return $"{greeting}\n{environmentDetail}\n(The key is {key})";
+        }
+    }
+}
---
+++ AzureAppConfigurationExample/Startup.cs
@@ -0,0 +1,69 @@
+namespace AzureAppConfigurationExample
+{
+    using Azure.Identity;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using System;
+
+    public class Startup
+    {
+        private readonly string Environment;
+
+        public Startup(IWebHostEnvironment env)
+        {
+            this.Environment = env.EnvironmentName;
+
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{this.Environment}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+            var appSettings = builder.Build();
+            var credentials = new DefaultAzureCredential();
+            var appConfigEndpoint = appSettings["ConnectionStrings:AppConfigEndpoint"];
+            var region = appSettings["Region"];
+            this.Configuration = builder
+                .AddAzureAppConfiguration(options => options
+                    .Connect(new Uri(appConfigEndpoint), credentials)
+                    .UseFeatureFlags()
+                    .Select(KeyFilter.Any, LabelFilter.Null)
+                    .Select(KeyFilter.Any, this.Environment)
+                    .Select(KeyFilter.Any, region)
+                    .ConfigureKeyVault(o => o.SetCredential(credentials))
+                    .ConfigureRefresh(o => o.SetCacheExpiration(TimeSpan.FromMinutes(1))))
+                .Build();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton(this.Configuration);
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
